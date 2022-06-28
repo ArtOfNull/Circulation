eea477e7aec1b7048da8a7c5bdab088f9ff71feb
AesenZero <AesenZero@ukr.net>
2022-06-29 00:14:32 +0200

    Level 2 Finished, Point system reworked

--- Circulation/Assets/_Scripts/DotsControlScript.cs
+++ Circulation/Assets/_Scripts/DotsControlScript.cs
@@ -25,22 +25,22 @@ public class DotsControlScript : MonoBehaviour
     [SerializeField]
     float cameraSpeed = 0.1f;
     bool isCamMove = false;
-    Vector3 offset = new Vector3(0f, -1.5f, 0f);
+    public Vector3 offset = new Vector3(0f, -1.5f, 0f);
     [SerializeField]
     List<int> RedWays = new List<int>();
     [SerializeField]
-    List<Transform> RedPoints = new List<Transform>();
+    List<PointScript> RedPoints = new List<PointScript>();
     [SerializeField]
     List<int> BlueWays = new List<int>();
     [SerializeField]
-    List<Transform> BluePoints = new List<Transform>();
+    List<PointScript> BluePoints = new List<PointScript>();
     List<int> curWays;
-    List<Transform> curPoints;
-    int counter = 0;
+    public PointScript curPoint;
+    public int counter = 0;
     bool moving = false;
     float savedSig = 0f;
     [SerializeField]
-    Transform lastPoint;
+    PointScript lastPoint;
     public bool finished = false;
 
     public int moves;
@@ -54,7 +54,6 @@ public class DotsControlScript : MonoBehaviour
             mainDot = BlueDot.transform;
             subDot = RedDot.transform;
             curWays = RedWays;
-            curPoints = RedPoints;
             state = !state;
             cameraChanger = 0f;
             camChModifier = 1;
@@ -67,7 +66,6 @@ public class DotsControlScript : MonoBehaviour
             mainDot = RedDot.transform;
             subDot = BlueDot.transform;
             curWays = BlueWays;
-            curPoints = BluePoints;
             state = !state;
             cameraChanger = 1f;
             camChModifier = -1;
@@ -80,7 +78,7 @@ public class DotsControlScript : MonoBehaviour
         mainDot = BlueDot.transform;
         subDot = RedDot.transform;
         curWays = RedWays;
-        curPoints = RedPoints;
+        curPoint = RedPoints[0];
         UIC = GetComponent<UIcontrollerScrpit>();
     }
 
@@ -92,21 +90,46 @@ public class DotsControlScript : MonoBehaviour
             //swapDotControl();
         }
         signal = Input.GetAxisRaw("Horizontal");
-        if ((signal == curWays[counter] || curWays[counter] == 2) && !moving && !finished && moves>0)
+        if ((signal == curWays[counter] || (curWays[counter] == 2 && signal != 0)) && !moving && !finished && moves>0)
         {
             savedSig = signal;
             moving = true;
             moves--;
 
+            if (!state)
+            {
+                foreach (PointScript poi in RedPoints)
+                {
+                    if (signal == poi.signal && poi.id-1 == counter)
+                    {
+                        curPoint = poi;
+                        break;
+                    }
+
+                }
+            }          
+            else
+            {
+                foreach (PointScript poi in BluePoints)
+                {
+                    if (signal == poi.signal && poi.id-1 == counter)
+                    {
+                        curPoint = poi;
+                        break;
+                    }
+
+                }
+            }
+
         }
         if (moving)
         {
             mainDot.transform.Rotate(Vector3.back * savedSig * rotationSpeed * Time.deltaTime);
-            if (Vector2.Distance(subDot.position, curPoints[counter].position)<=0.5f)
+            if (Vector2.Distance(subDot.position, curPoint.gameObject.transform.position)<=0.5f)
             {
                 moving = false;
-                subDot.position = curPoints[counter].position;
-                if(curPoints[counter] != lastPoint)
+                subDot.position = curPoint.gameObject.transform.position;
+                if(curPoint != lastPoint)
                 {
                     if (mainDot == RedDot.transform) counter++;                    
                     swapDotControl();
@@ -123,8 +146,8 @@ public class DotsControlScript : MonoBehaviour
 
         print(signal);
         // mainDot.transform.Rotate(Vector3.back * signal * rotationSpeed * Time.deltaTime);
-        cameraChanger += cameraSpeed * camChModifier * Time.deltaTime;
-        Vector2 newCamPos = Vector2.Lerp(RedDot.transform.position, BlueDot.transform.position, cameraChanger);
-        Camera.main.transform.position = new Vector3(newCamPos.x, newCamPos.y, Camera.main.transform.position.z) + offset;
+        //cameraChanger += cameraSpeed * camChModifier * Time.deltaTime;
+        //Vector2 newCamPos = Vector2.Lerp(RedDot.transform.position, BlueDot.transform.position, cameraChanger);
+        //Camera.main.transform.position = new Vector3(newCamPos.x, newCamPos.y, Camera.main.transform.position.z) + offset;
     }
 }
---
+++ Circulation/Assets/_Scripts/PointScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointScript : MonoBehaviour
+{
+    public int id;
+    public int signal = 1;
+    public bool isBlue = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
